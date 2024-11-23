c2d4f7f2d3dc309e68368de9b963d93068aac6ff
FatimaAflous <aflousfatima@gmail.com>
2024-11-23 19:36:54 +0100

    modify the region variable

--- variables.tf
+++ variables.tf
@@ -15,5 +15,5 @@ default = "dev-fplarache-smartlib-catalog-fgservice-faf"
 }
 variable "subnet_ids" {
 type = list(string)
-default     = ["subnet-0cf9e8ccfb6fd0f77", "subnet-05fac6b2678b41a37", "subnet-00d967507df4b0bfb"]
+default = ["subnet-0cf9e8ccfb6fd0f77", "subnet-05fac6b2678b41a37", "subnet-00d967507df4b0bfb"]
 }
