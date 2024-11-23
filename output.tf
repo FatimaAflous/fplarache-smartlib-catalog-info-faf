22d69c46cf2f470cbb6c79605c1bc1463aa95638
FatimaAflous <aflousfatima@gmail.com>
2024-11-23 19:20:20 +0100

    add service name to the output

--- output.tf
+++ output.tf
@@ -4,3 +4,6 @@ output "ecr_repository_url" {
 output "ecs_cluster_id" {
     value = aws_ecs_cluster.catalog_cluster.id
 }
+output "ecs_service_name" {
+    value = aws_ecs_service.catalog_service.name
+}
