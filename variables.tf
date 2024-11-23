e730687941e6cf2dfd599cae73baee24b37da31c
FatimaAflous <aflousfatima@gmail.com>
2024-11-23 20:17:20 +0100

    resolve a missing variable problem in variable.tf

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ default = "dev-fplarache-smartlib-catalog-fgcluster-faf"
 variable "ecs_task_family"{
 default = "dev-fplarache-smartlib-catalog-td-faf"
 }
-variable "ecr_service_name"{
+variable "ecs_service_name"{
 default = "dev-fplarache-smartlib-catalog-fgservice-faf"
 }
-variable "subnet_ids" {
+variable "subnets_ids" {
 type = list(string)
 default = ["subnet-0cf9e8ccfb6fd0f77", "subnet-05fac6b2678b41a37", "subnet-00d967507df4b0bfb"]
 }
